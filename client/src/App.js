ec0c5bbcef5d985587571a6fdf235e93180f6dc1
Rajneesh100 <75190067+Rajneesh100@users.noreply.github.com>
2023-06-13 10:31:41 +0530

    Add files via upload

---
+++ client/src/App.js
@@ -0,0 +1,29 @@
+import { lazy, Suspense } from 'react';
+
+import { GoogleOAuthProvider } from '@react-oauth/google';
+
+//components
+import UserProvider from './context/UserProvider';
+import AccountProvider from './context/AccountProvider';
+
+import Loader from './components/loader/Loader';
+
+const Messenger = lazy(() => import('./components/Messenger'));
+
+function App() {
+
+  const clientId = '699222427046-ajumh4mbvdht0d0s8srg6eoer1u2dfvi.apps.googleusercontent.com';
+  return (
+    <GoogleOAuthProvider clientId={clientId}>
+      <UserProvider>
+        <AccountProvider>
+          <Suspense fallback={<Loader />}>
+            <Messenger/>
+          </Suspense>
+        </AccountProvider>
+      </UserProvider>
+    </GoogleOAuthProvider>
+  );
+}
+
+export default App;
